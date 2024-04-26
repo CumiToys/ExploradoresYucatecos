23cb0b496c9df2f9c13f8ccb854f389fd149add6
Mario Pech <pech.mivan@gmail.com>
2024-04-25 18:47:50 -0600

    removed unnecesary styles and elements and added a new div to display each question

--- script.js
+++ script.js
@@ -6,40 +6,4 @@ function generateRandomNumber() {
 
     // Muestra el número generado en el elemento con id "randomNumber"
     document.getElementById('randomNumber').textContent = randomNumber;
-
-    // Asigna un texto asociado al número
-    var text = "";
-    switch (randomNumber) {
-        case 1:
-            text = "Texto asociado al número 1";
-            break;
-        case 2:
-            text = "Texto asociado al número 2";
-            break;
-        case 3:
-            text = "Texto asociado al número 3";
-            break;
-        case 4:
-            text = "Texto asociado al número 4";
-            break;
-        case 5:
-            text = "Texto asociado al número 5";
-            break;
-        case 6:
-            text = "Texto asociado al número 6";
-            break;
-        case 7:
-            text = "Texto asociado al número 7";
-            break;
-        case 8:
-            text = "Texto asociado al número 8";
-            break;
-        case 9:
-            text = "Texto asociado al número 9";
-            break;
-        case 10:
-            text = "Texto asociado al número 10";
-            break;
-    }
-    document.getElementById('randomText').textContent = text;
 }
