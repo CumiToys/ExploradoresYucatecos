e985d4356bfd18a2bf508fb7a56a9e5cd547b686
Mario Pech <pech.mivan@gmail.com>
2024-04-27 19:51:52 -0600

    added custom effects and different settings for different screen sizes

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById('generateButton').addEventListener('click', generateRandomNumber);
+var loadGameButton = document.getElementById('generateButton');
+loadGameButton.addEventListener('click', loadGame);
+var radioInputs = document.querySelectorAll('.answerOption');
+var answerButton = document.getElementById('answerButton');
+var questionBox = document.getElementById('questionBox');
+var questionBoxContent = document.getElementById('questionBoxContent')
+
+function loadGame() {
+    generateRandomNumber();
+    answerButton.disabled = true;
+    questionBox.classList.add('showBox');
+    loadGameButton.style.display = 'none';
+    questionBoxContent.classList.remove('hidden');
+}
 
 function generateRandomNumber() {
     // Genera un número aleatorio del 1 al 10
@@ -6,4 +19,14 @@ function generateRandomNumber() {
 
     // Muestra el número generado en el elemento con id "randomNumber"
     document.getElementById('randomNumber').textContent = randomNumber;
+}
+
+radioInputs.forEach(function(radio) {
+    radio.addEventListener('change', function() {
+        handleOptionChange(this);
+    });
+});
+
+function handleOptionChange(radio) {
+    answerButton.disabled = false;
 }
