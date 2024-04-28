54a7f73675ecc89e70eab46d888431b62dbd338e
Mario Pech <pech.mivan@gmail.com>
2024-04-28 17:16:56 -0600

    added load game functionality

--- script.js
+++ script.js
@@ -1,34 +1,87 @@
-var loadGameButton = document.getElementById('generateButton');
+var loadGameButton = document.getElementById('startButton');
 loadGameButton.addEventListener('click', loadGame);
-var radioInputs = document.querySelectorAll('.answerOption');
-var answerButton = document.getElementById('answerButton');
-var questionNumberBox = document.getElementById('questionNumberBox');
-var questionBox = document.getElementById('questionBox');
-var questionBoxContent = document.getElementById('questionBoxContent')
 
-function loadGame() {
-    generateRandomNumber();
-    answerButton.disabled = true;
+var quizz = [];
+var number = 0;
+
+async function loadGame() {
+    setActionEvents();
+    await loadData();
+    randomNumber = generateRandomNumber(quizz.length);
+    displayQuestionBox();
+    showContent();
+    loadQuestion(randomNumber);
+}
+
+function setActionEvents() {
+    var radioInputs = document.querySelectorAll('.answerOption');
+
+    radioInputs.forEach(function(radio) {
+        radio.addEventListener('change', function() {
+            handleOptionChange(this);
+        });
+    });
+}
+
+function displayQuestionBox() {
+    var questionBox = document.getElementById('questionBox');
+    
     questionBox.classList.remove('hidden');
     questionBox.classList.add('visible');
+}
+
+function showContent() {
+    var questionBoxContent = document.getElementById('questionBoxContent')
+
     questionBoxContent.classList.remove('hidden');
     questionBoxContent.classList.add('visible');
 }
 
-function generateRandomNumber() {
-    // Genera un número aleatorio del 1 al 10
-    var randomNumber = Math.floor(Math.random() * 10) + 1;
+function loadQuestion(questionNumber) {
+    var questionBoxContent = document.getElementById('questionBoxContent')
+    var answerButton = document.getElementById('answerButton');
 
-    // Muestra el número generado en el elemento con id "randomNumber"
-    document.getElementById('randomNumber').textContent = randomNumber;
+    questionBoxContent.classList.add('fade-away');
+    fillContent(questionBoxContent, questionNumber);
+    answerButton.disabled = true;
+    questionBoxContent.classList.add('fade-in');
 }
 
-radioInputs.forEach(function(radio) {
-    radio.addEventListener('change', function() {
-        handleOptionChange(this);
+function fillContent(content, questionNumber) {
+    var questionNumberBox = document.getElementById('questionNumber');
+    var questionText = content.querySelector('#questionText');
+    var options = content.querySelectorAll('.answerOption');
+
+    questionNumberBox.textContent = questionNumber + 1;
+    
+    questionText.textContent = quizz[questionNumber].text;
+    options.forEach((option, index) => {
+        var questionOption = quizz[questionNumber].options[index];
+        option.value = questionOption.value;
+        option.nextSibling.nodeValue = questionOption.text;
     });
-});
+}
+
+function generateRandomNumber(maxValue) {
+    // Genera un número aleatorio del 0 al valor máximo(maxValue - 1).
+    return Math.floor(Math.random() * maxValue);
+}
 
 function handleOptionChange(radio) {
     answerButton.disabled = false;
+    console.log(radio.value);
+}
+
+async function loadData() {
+    try {
+        const response = await fetch('questions.json');
+        if (!response.ok) {
+          throw new Error('File Not Found');
+        }
+        quizz = await response.json();
+        return quizz;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+      }
 }
