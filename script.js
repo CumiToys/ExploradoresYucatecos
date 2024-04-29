9bb0c25a2fd6d67a4800b4030f5d2f83e4ecb27d
Mario Pech <pech.mivan@gmail.com>
2024-04-29 14:08:31 -0600

    added check answer functionality

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var loadGameButton = document.getElementById('startButton');
 loadGameButton.addEventListener('click', loadGame);
 
 var quizz = [];
+var questionAnswered = false;
 
 async function loadGame() {
-    setActionEvents();
     await loadData();
+    setActionEvents();
     displayQuestionBox();
     loadNextQuestion();
     showContent();
@@ -13,7 +14,8 @@ async function loadGame() {
 
 function setActionEvents() {
     var radioInputs = document.querySelectorAll('.answerOption');
-    var nextQuestionButton = document.getElementById('nextQuestionButton'); 
+    var nextQuestionButton = document.getElementById('nextQuestionButton');
+    var answerButton = document.getElementById('answerButton');
 
     radioInputs.forEach(function(radio) {
         radio.addEventListener('change', function() {
@@ -22,43 +24,57 @@ function setActionEvents() {
     });
 
     nextQuestionButton.addEventListener('click', loadNextQuestion);
+    answerButton.addEventListener('click', checkAnswer);
 }
 
 function displayQuestionBox() {
     var questionBox = document.getElementById('questionBox');
-    
-    questionBox.classList.remove('hidden');
-    questionBox.classList.add('visible');
+    show(questionBox);
 }
 
 function showContent() {
     var questionBoxContent = document.getElementById('questionBoxContent')
-
-    questionBoxContent.classList.remove('hidden');
-    questionBoxContent.classList.add('visible');
+    show(questionBoxContent);
 }
 
 function loadNextQuestion() {
-    var randomNumber = generateRandomNumber(quizz.length);
-    loadQuestion(randomNumber);
+    if (questionAnswered) {
+        resetSettings();
+        questionAnswered = false;
+    }
+    
+    if (quizz.length > 0) {
+        var randomNumber = generateRandomNumber(quizz.length);
+        loadQuestion(randomNumber);
+    } else {
+        console.log('game over!');
+    }
+}
+
+function resetSettings() {
+    var questionBox = document.getElementById('questionBox');
+    var radioInputs = document.querySelectorAll('.answerOption');
+
+    var questionResult = setQuestionResult('', 'white');
+    hide(questionResult);
+    setBackgroundColor(questionBox, 'white');
+    enableMany(radioInputs);
 }
 
 function loadQuestion(questionNumber) {
     var questionBoxContent = document.getElementById('questionBoxContent')
     var answerButton = document.getElementById('answerButton');
 
-    answerButton.disabled = true;
-    questionBoxContent.classList.add('fade-away');
+    disable(answerButton);
     fillContent(questionBoxContent, questionNumber);
-    questionBoxContent.classList.add('fade-in');
 }
 
 function fillContent(content, questionNumber) {
-    var questionNumberBox = document.getElementById('questionNumber');
+    var portraitBox = document.getElementById('questionNumber');
     var questionText = content.querySelector('#questionText');
     var options = content.querySelectorAll('.answerOption');
 
-    questionNumberBox.textContent = questionNumber + 1;
+    portraitBox.textContent = questionNumber + 1;
     
     questionText.textContent = quizz[questionNumber].text;
     options.forEach((option, index) => {
@@ -69,6 +85,48 @@ function fillContent(content, questionNumber) {
     });
 }
 
+function checkAnswer() {
+    var questionBox = document.getElementById('questionBox');
+    var selectedOption = document.querySelector('input[class="answerOption"]:checked');
+    var questionNumber = document.getElementById('questionNumber').textContent;
+    var question = quizz[questionNumber - 1];
+    var questionResult;
+
+    questionAnswered = true;
+    lockAnswer();
+
+    if(selectedOption.value == question.rightAnswer){
+        setBackgroundColor(questionBox, '#b0f9de');
+        questionResult = setQuestionResult(question.rightAnswerOutput, '#18ba69');
+        quizz.splice(questionNumber - 1, 1);
+    } else {
+        setBackgroundColor(questionBox, '#f3aaaa');
+        questionResult = setQuestionResult(question.wrongAnswerOutput, '#c42b2b');
+    }
+
+    show(questionResult);
+}
+
+function setQuestionResult(result, color) {
+    var questionResult = document.getElementById('questionResult');
+
+    questionResult.textContent = result;
+    questionResult.style.color = color;
+    return questionResult;
+}
+
+function setBackgroundColor(element, color) {
+    element.style.backgroundColor = color;
+}
+
+function lockAnswer() {
+    var radioInputs = document.querySelectorAll('.answerOption');
+    var answerButton = document.getElementById('answerButton');
+
+    disableMany(radioInputs);
+    disable(answerButton);
+}
+
 function generateRandomNumber(maxValue) {
     // Genera un número aleatorio del 0 al valor máximo(maxValue - 1).
     return Math.floor(Math.random() * maxValue);
@@ -76,7 +134,36 @@ function generateRandomNumber(maxValue) {
 
 function handleOptionChange(radio) {
     answerButton.disabled = false;
-    console.log(radio.value);
+}
+
+function hide(element) {
+    element.classList.remove('visible');
+    element.classList.add('hidden');
+}
+
+function show(element) {
+    element.classList.remove('hidden');
+    element.classList.add('visible');
+}
+
+function disable(element) {
+    element.disabled = true;
+}
+
+function disableMany(elements) {
+    elements.forEach(element => {
+        disable(element);
+    });
+}
+
+function enable(element) {
+    element.disabled = false;
+}
+
+function enableMany(elements) {
+    elements.forEach(element => {
+        enable(element);
+    });
 }
 
 async function loadData() {
@@ -89,6 +176,5 @@ async function loadData() {
         return quizz;
       } catch (error) {
         console.error('Error fetching data:', error);
-        return [];
       }
 }
